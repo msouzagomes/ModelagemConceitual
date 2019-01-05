c79ffa53a9c9b3fd65a1f5410e3dfacfb7437602
msouzagomes <msouzagomes@terra.com.br>
2019-01-05 13:05:02 -0200

    Projeto Criado

---
+++ src/main/java/com/modelagem/br/ModelagemApplication.java
@@ -0,0 +1,14 @@
+package com.modelagem.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelagemApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/modelagem/br/ModelagemApplicationTests.java
@@ -0,0 +1,17 @@
+package com.modelagem.br;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModelagemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
