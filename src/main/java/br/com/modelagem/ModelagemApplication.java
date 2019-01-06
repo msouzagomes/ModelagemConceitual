9a458bc3ca9287ef27e2e2ae0197d6bd475b84aa
msouzagomes <msouzagomes@terra.com.br>
2019-01-06 13:42:36 -0200

    Opreção Instanciação

--- src/main/java/br/com/modelagem/ModelagemApplication.java
+++ src/main/java/br/com/modelagem/ModelagemApplication.java
@@ -1,13 +1,30 @@
 package br.com.modelagem;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.modelagem.domain.Categoria;
+import br.com.modelagem.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ModelagemApplication {
+public class ModelagemApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
