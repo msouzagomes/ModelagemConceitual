5660d71279b676a0d35bf1fd8ac68f69db4a641a
msouzagomes <msouzagomes@terra.com.br>
2019-01-06 14:14:12 -0200

    Produto e associacao muitos para muitos

--- src/main/java/br/com/modelagem/ModelagemApplication.java
+++ src/main/java/br/com/modelagem/ModelagemApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.modelagem.domain.Categoria;
+import br.com.modelagem.domain.Produto;
 import br.com.modelagem.repositories.CategoriaRepository;
+import br.com.modelagem.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ModelagemApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemApplication.class, args);
@@ -22,9 +27,23 @@ public class ModelagemApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		this.produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
